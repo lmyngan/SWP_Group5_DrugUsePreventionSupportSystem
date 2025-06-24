bade9154a56b86624f99397f2ea50d1f2599bdf7
ChesterK01 <tuandnse173227@fpt.edu.vn>
2025-06-24 20:35:55 +0700

    update get account

--- back-end/DrugsPrevention_Data/DTO/Account/AccountResponseDTO.cs
+++ back-end/DrugsPrevention_Data/DTO/Account/AccountResponseDTO.cs
@@ -9,6 +9,12 @@ namespace DrugsPrevention_Data.DTO.Account
     public class AccountResponseDTO
     {
         public int AccountId { get; set; }
+        public string Accountname { get; set; }
         public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public int RoleId { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
 }
