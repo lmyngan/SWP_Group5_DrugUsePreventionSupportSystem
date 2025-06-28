7bcdeafa0942702ceeb9f166ef0a0a1296d85317
trung <trungdmhe180195@fpt.edu.vn>
2025-06-28 20:29:09 +0700

    implement feature blog

--- back-end/DrugsPrevention/Program.cs
+++ back-end/DrugsPrevention/Program.cs
@@ -45,6 +45,8 @@ namespace DrugsPrevention
             builder.Services.AddScoped<IAccountRepository, AccountRepository>();
             builder.Services.AddScoped<IEventService, EventService>();
             builder.Services.AddScoped<IEventRepository, EventRepository>();
+            builder.Services.AddScoped<IBlogService, BlogService>();
+            builder.Services.AddScoped<IBlogRepository, BlogRepository>();
 
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
