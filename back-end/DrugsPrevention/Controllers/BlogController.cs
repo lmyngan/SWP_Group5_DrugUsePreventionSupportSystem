97962dab8389b125d790282e438500594c3cd542
trung <trungdmhe180195@fpt.edu.vn>
2025-06-28 21:27:02 +0700

    implement event and blog

---
+++ back-end/DrugsPrevention/Controllers/BlogController.cs
@@ -0,0 +1,68 @@
+﻿using DrugsPrevention_Data.DTO.Blog;
+using DrugsPrevention_Service.Service.Iservice;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DrugsPrevention_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly IBlogService _blogService;
+
+        public BlogController(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var blogs = await _blogService.GetAllAsync();
+            return Ok(blogs);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var blog = await _blogService.GetByIdAsync(id);
+            if (blog == null) return NotFound();
+            return Ok(blog);
+        }
+
+        [HttpGet("details/{id}")]
+        public async Task<IActionResult> GetDetails(int id)
+        {
+            var blog = await _blogService.GetByIdAsync(id);
+            if (blog == null)
+                return NotFound();
+
+            return Ok(blog);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateBlogDto dto)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            await _blogService.AddAsync(dto);
+            return Ok(new { message = "Blog created successfully." });
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] UpdateBlogDto dto)
+        {
+            if (id != dto.BlogId) return BadRequest("ID mismatch.");
+            await _blogService.UpdateAsync(dto);
+            return Ok(new { message = "Blog updated successfully." });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _blogService.DeleteAsync(id);
+            return Ok(new { message = "Blog deleted successfully." });
+        }
+    }
+
+}
---
+++ back-end/DrugsPrevention_Service/Service/Iservice/IEventService.cs
@@ -0,0 +1,20 @@
+﻿using DrugsPrevention_Data.Data;
+using DrugsPrevention_Data.DTO.Event;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrugsPrevention_Service.Service.Iservice
+{
+    public interface IEventService
+    {
+        Task<IEnumerable<EventDto>> GetAllAsync();
+        Task<EventDto> GetByIdAsync(int id);
+        Task<EventDto> GetWithDetailsAsync(int id);
+        Task AddAsync(CreateEventDto dto);
+        Task UpdateAsync(UpdateEventDto dto);
+        Task DeleteAsync(int id);
+    }
+}
