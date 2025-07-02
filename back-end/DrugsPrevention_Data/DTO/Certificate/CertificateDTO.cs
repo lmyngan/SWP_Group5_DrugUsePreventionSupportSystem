9c9dc1c9fdbfb6e37df632e825b3ce54acb6bb3c
ChesterK01 <tuandnse173227@fpt.edu.vn>
2025-07-02 11:19:23 +0700

    added crud for certificate

---
+++ back-end/DrugsPrevention_Data/DTO/Certificate/CertificateDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrugsPrevention_Data.DTO.Certificate
+{
+    public class CertificateDTO
+    {
+        public int CertificateId { get; set; }
+        public int ConsultantId { get; set; }
+        public string CertificateName { get; set; }
+    }
+}
---
+++ back-end/DrugsPrevention_Data/DTO/Certificate/CreateCertificateDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrugsPrevention_Data.DTO.Certificate
+{
+    public class CreateCertificateDTO
+    {
+        public int ConsultantId { get; set; }
+        public string CertificateName { get; set; }
+    }
+}
