a8e30faddaa7c7f43e012c4a2c79c6f84bd1fe01
ThaoTNYSE161001 <thaotnyse161001@fpt.edu.vn>
2025-07-30 22:09:39 +0700

    add search bar in event

--- front-end/src/pages/EventPage.js
+++ front-end/src/pages/EventPage.js
@@ -140,18 +140,24 @@ const EventPage = ({ navigateTo }) => {
           {/* Search Bar */}
           <div className="search-container">
             <div className="search-input-wrapper">
+              <button className="search-icon-btn">
+                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M21 21L16.514 16.506L21 21ZM19 10.5C19 15.194 15.194 19 10.5 19C5.806 19 2 15.194 2 10.5C2 5.806 5.806 2 10.5 2C15.194 2 19 5.806 19 10.5Z"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </button>
               <input
                 type="text"
-                placeholder="Search events by name..."
+                placeholder="Search..."
                 value={searchQuery}
                 onChange={(e) => setSearchQuery(e.target.value)}
                 className="search-input"
               />
-              {searchQuery && (
-                <button onClick={clearSearch} className="clear-search-btn">
-                  ✕
-                </button>
-              )}
             </div>
           </div>
 
