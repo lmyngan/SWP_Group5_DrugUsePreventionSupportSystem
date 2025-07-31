07bcad9197c209a8e1c67e8793a24d42544e201d
ChesterK01 <tuandnse173227@fpt.edu.vn>
2025-07-31 10:44:26 +0700

    added email into register

--- back-end/DrugsPrevention_Data/DTO/Register/RegisterRequestDTO.cs
+++ back-end/DrugsPrevention_Data/DTO/Register/RegisterRequestDTO.cs
@@ -13,6 +13,7 @@ namespace DrugsPrevention_Data.DTO.Register
         public string FullName { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string Gender { get; set; }
+        public string Email { get; set; }
         public string Address { get; set; }
         public string RoleName { get; set; }
     }
